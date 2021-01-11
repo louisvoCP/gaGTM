1909898db8d3d647b6a3207aad7e6d607ddf4cba
Louis Vo <louis.vo@cardinalpath.com>
2021-01-11 14:34:08 -0500

    new start

---
+++ app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCadpa1mkOLfrU8aUgPUqJYQNx4DTN0sjs",
+    authDomain: "astute-asset-244919.firebaseapp.com",
+    databaseURL: "https://astute-asset-244919.firebaseio.com",
+    projectId: "astute-asset-244919",
+    storageBucket: "astute-asset-244919.appspot.com",
+    messagingSenderId: "714582457983",
+    appId: "1:714582457983:web:51f3b29f55ba528501094a",
+    measurementId: "G-4274GV0PB4"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
